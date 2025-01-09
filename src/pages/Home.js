f6e123f95fa2a10eb61b137e50104e31739a697c
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2025-01-09 11:07:01 +0100

    context and hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
-import { useContext } from "react";
-
-import { CounterContext } from "../context/CounterContext";
+// import { useContext } from "react";
 import ChangeCounter from "../components/ChangeCounter";
+// import { CounterContext } from "../context/CounterContext";
+
+// 4 - refactoring the hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const Home = () => {
-  const { counter } = useContext(CounterContext);
+  // const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
   return (
     <div>
       <h1>Home</h1>
