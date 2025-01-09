59e4e0d7242c9b1b2f2478470bf8f361b2af7b33
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2025-01-09 17:19:58 +0100

    changing context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,19 @@ import ChangeCounter from "../components/ChangeCounter";
 // 4 - refactoring the hook
 import { useCounterContext } from "../hooks/useCounterContext";
 
+// 5 - complex context
+import { useTitleColorContext } from "../hooks/useTitltleColorContext";
+
 const Home = () => {
   // const { counter } = useContext(CounterContext);
   const { counter } = useCounterContext();
+
+  // 5 - complex context
+  const { color } = useTitleColorContext();
+  console.log(color);
   return (
     <div>
-      <h1>Home</h1>
+      <h1 style={{ color: color }}>Home</h1>
       <p>Counter value: {counter}</p>
       {/* 3 - changing context value */}
       <ChangeCounter />
